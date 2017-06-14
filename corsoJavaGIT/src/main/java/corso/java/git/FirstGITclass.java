b8feeef89a89197c45f8abfe1d9bf414e9cb705d
pietroromagnoli <pietroromagnoli@gmail.com>
2017-06-14 10:16:59 +0200

    indovina la funzione

--- corsoJavaGIT/src/main/java/corso/java/git/FirstGITclass.java
+++ corsoJavaGIT/src/main/java/corso/java/git/FirstGITclass.java
@@ -23,4 +23,14 @@ public class FirstGITclass {
 		return "Hello " + name;
 	}
 	
+	public int boh(int[] a){
+		
+		int x=0;
+		for (int i=0;i<a.length;i++){
+			if (a[i]>x)
+				x=a[i];				
+	}
+		return x;
+	
+}
 }
