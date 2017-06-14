6d474732383ed907f2cd816b63162acf9679c5d2
pietroromagnoli <pietroromagnoli@gmail.com>
2017-06-14 10:43:18 +0200

    questo è un push del branch di pietro

--- corsoJavaGIT/src/main/java/corso/java/git/FirstGITclass.java
+++ corsoJavaGIT/src/main/java/corso/java/git/FirstGITclass.java
@@ -23,39 +23,8 @@ public class FirstGITclass {
 		return "Hello " + name;
 	}
 	
-	public int boh(int[] a){
-		
-		int x=0;
-		for (int i=0;i<a.length;i++){
-			if (a[i]>x)
-				x=a[i];				
-	}
-		return x;
 	
-	}
 	
-	  public void bubbleSort(int [] array) {
-
-	        for(int i = 0; i < array.length; i++) {
-	            boolean flag = false;
-	            for(int j = 0; j < array.length-1; j++) {
-
-	                //Se l' elemento j e maggiore del successivo allora
-	                //scambiamo i valori
-	                if(array[j]>array[j+1]) {
-	                    int k = array[j];
-	                    array[j] = array[j+1];
-	                    array[j+1] = k;
-	                    flag=true; //Lo setto a true per indicare che é avvenuto uno scambio
-	                }
-	                
-
-	            }
-
-	            if(!flag) break; //Se flag=false allora vuol dire che nell' ultima iterazione
-	                             //non ci sono stati scambi, quindi il metodo può terminare
-	                             //poiché l' array risulta ordinato
-	        }
-	    }
+	 
 
 }
