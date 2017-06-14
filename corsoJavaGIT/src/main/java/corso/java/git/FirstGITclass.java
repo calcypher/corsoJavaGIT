0a57d1bc4160c6e10c3e0b213e787a6bc00e0ea9
WeedMaster <victorbolado_@hotmail.com>
2017-06-14 10:43:14 +0200

    sasa

--- corsoJavaGIT/src/main/java/corso/java/git/FirstGITclass.java
+++ corsoJavaGIT/src/main/java/corso/java/git/FirstGITclass.java
@@ -2,37 +2,13 @@ package corso.java.git;
 
 public class FirstGITclass {
 
-	public void firstMethod() {
 
-		System.out.println("Test secondo commi");
-
-		System.out.println("sono sempre Pietro");
-		
-
-	}
-	
-	public void ciao() {
-		
-	}
-	
-	public void method2() {
-		
-	}
 	
 	public String helloName(String name){
 		return "Hello " + name;
 	}
+
 	
-	public int boh(int[] a){
-		
-		int x=0;
-		for (int i=0;i<a.length;i++){
-			if (a[i]>x)
-				x=a[i];				
-	}
-		return x;
-	
-	}
 	
 	  public void bubbleSort(int [] array) {
 
